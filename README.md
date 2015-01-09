95b67d6d3b1a1af3041771171c86042bfb9de033
vzvu3k6k <vzvu3k6k@gmail.com>
2015-01-10 00:50:41 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# See Also
+
+## [NARKOZ/crank-for-rubygems](https://github.com/NARKOZ/crank-for-rubygems)
+
+A chrome-extension. <q cite="http://qiita.com/NARKOZ/items/48129b3c76690b324e92">Wherever you browse 'Gemfile' or '.gemspec' file on Github, clicking on gem name takes you to it's Github repository page.</q>
+
+[chrome-extension - gemのGitHubリポジトリへ簡単にアクセスできるChromeエクステンションを作りました（英語） - Qiita](http://qiita.com/NARKOZ/items/48129b3c76690b324e92)
+
+## [azu/github-display-package-dependencies](https://github.com/azu/github-display-package-dependencies)
+
+A userscript to display package.json dependencies in the header of GitHub repositories.
---
+++ github-linkify_gems_in_gemspec_gemfile.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name           GitHub: Linkify gems in *.gemspec and Gemfile
+// @version        0.1
+// @author         vzvu3k6k
+// @match          https://github.com/*
+// @namespace      http://vzvu3k6k.tk/
+// @license        CC0
+// ==/UserScript==
+
+var gemUrl = function(gemName){
+  return 'http://sla.herokuapp.com/' + gemName;
+};
+
+var filters = {
+  gemspec: function(node){
+    if (!node.previousSibling) return false;
+    return /spec\.add_(?:development_|runtime_)?dependency/.test(node.previousSibling.textContent);
+  },
+  gemfile: function(node){
+    if (!node.previousSibling) return false;
+    if (!node.previousSibling.previousSibling) return false;
+    return /gem/.test(node.previousSibling.previousSibling.textContent);
+  }
+};
+
+var tryLinkify = function (){
+  var filePath = document.querySelector('.final-path');
+  if (!filePath) return;
+  filePath = filePath.textContent;
+
+  var filter;
+  if (/\.gemspec$/.test(filePath)) {
+    filter = filters.gemspec;
+  } else if (filePath === 'Gemfile') {
+    filter = filters.gemfile;
+  } else {
+    return;
+  }
+
+  var labels = Array.prototype.filter.call(
+    document.querySelector('.file .blob-wrapper').querySelectorAll('.pl-s1'),
+    filter
+  );
+
+  labels.forEach(function(node){
+    var textNode = node.childNodes[1];
+    var a = document.createElement('a');
+    a.appendChild(textNode.cloneNode());
+    a.href = gemUrl(textNode.textContent);
+    node.replaceChild(a, textNode);
+  });
+};
+
+tryLinkify();
